6ce1419e0a5482642ee8d3643019ca48d3aec2b5
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-08-14 18:17:11 +0800

    Starting an acceptance test

---
+++ FreshCopy.Tests/AcceptanceTests/SimpleHappyPath.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace FreshCopy.Tests.AcceptanceTests
+{
+    [Trait("Happy Path", "Acceptance")]
+    public class SimpleHappyPath
+    {
+        [Fact(DisplayName = "Updates the target")]
+        public async Task Updatesthetarget()
+        {
+            var 
+        }
+    }
+}
