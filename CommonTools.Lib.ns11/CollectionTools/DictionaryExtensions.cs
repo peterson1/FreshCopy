79d9dc57ee989a551fa83870aed9d95d256b3572
Peterson Salamat <petersonsalamat@gmail.com>
2017-08-31 05:32:07 +0800

    DictionaryExtension: IfFound

---
+++ CommonTools.Lib.ns11/CollectionTools/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CommonTools.Lib.ns11.ExceptionTools;
+using System;
+using System.Collections.Generic;
+
+namespace CommonTools.Lib.ns11.CollectionTools
+{
+    public static class DictionaryExtensions
+    {
+        public static void IfFound<TKey, TVal>(this Dictionary<TKey, TVal> dict, TKey key, Action<TVal> action)
+        {
+            if (dict.TryGetValue(key, out TVal value))
+                action(value);
+            else
+                throw Fault.BadData($"Dictionary ‹{typeof(TVal).Name}› does not contain key [{key}].");
+        }
+    }
+}
