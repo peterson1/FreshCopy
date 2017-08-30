6f2978ab6ff851e7a9f1aa38e38874722062a07f
Peterson Salamat <petersonsalamat@gmail.com>
2017-08-31 05:31:42 +0800

    K-V store for local user

---
+++ CommonTools.Lib.fx45/LiteDbTools/LocalUserKeyValueStore1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace CommonTools.Lib.fx45.LiteDbTools
+{
+    public abstract class LocalUserKeyValueStore1 : LiteDbKeyValueStore1
+    {
+        public LocalUserKeyValueStore1(string fileName, string subDir = "CommonTools.Lib") 
+            : base(FindLocalUserPath(fileName, subDir))
+        {
+        }
+
+
+        private static string FindLocalUserPath(string fileName, string subDir)
+        {
+            var special = Environment.SpecialFolder.LocalApplicationData;
+            var baseDir = Environment.GetFolderPath(special);
+            var fullDir = Path.Combine(baseDir, subDir);
+            Directory.CreateDirectory(fullDir);
+            return Path.Combine(fullDir, fileName);
+        }
+    }
+}
