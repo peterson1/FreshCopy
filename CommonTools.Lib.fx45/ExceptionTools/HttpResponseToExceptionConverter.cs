e642bf1ab0b2e84d8c28c1379bc5a85c57c13278
Peterson Salamat <petersonsalamat@gmail.com>
2017-09-02 05:45:30 +0800

    Abstracted out custom exception to wrap unexpected HttpWebResponses

---
+++ CommonTools.Lib.fx45/ExceptionTools/HttpResponseToExceptionConverter.cs
@@ -0,0 +1,13 @@
+﻿using CommonTools.Lib.ns11.ExceptionTools;
+using System.Net;
+
+namespace CommonTools.Lib.fx45.ExceptionTools
+{
+    public static class HttpResponseToExceptionConverter
+    {
+        public static UnexpectedResponseException ToUnexpectedException(this HttpWebResponse resp, HttpStatusCode expected)
+            => new UnexpectedResponseException(resp.StatusDescription, 
+                                               expected.ToString(), 
+                                               resp.StatusCode.ToString());
+    }
+}
---
+++ CommonTools.Lib.ns11/ExceptionTools/UnexpectedResponseException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+using CommonTools.Lib.ns11.StringTools;
+
+namespace CommonTools.Lib.ns11.ExceptionTools
+{
+    public class UnexpectedResponseException : Exception
+    {
+        public UnexpectedResponseException(string message,
+                                           string expectedResponse,
+                                           string actualResponse) 
+            : base(Compose(expectedResponse, actualResponse, message))
+        {
+        }
+
+
+        private static string Compose(string expectedResponse, string actualResponse, string message)
+            => $"{message}{L.f}expected: “{expectedResponse}”"
+                      + $"{L.f}  actual: “{actualResponse}”";
+    }
+}
