d77cb370e7782b693911ef0b08c62a3041bc8e13
Peterson Salamat <petersonsalamat@gmail.com>
2017-09-07 20:13:41 +0800

    Added UIList as a wrapper for ObservableCollections with SummaryRows

---
+++ CommonTools.Lib.ns11/DataStructures/UIList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CommonTools.Lib.ns11.DataStructures
+{
+    public class UIList<T> : ObservableCollection<T>
+    {
+        //private      EventHandler _summaryRowAdded;
+        //public event EventHandler  SummaryRowAdded
+        //{
+        //    add    { _summaryRowAdded -= value; _summaryRowAdded += value; }
+        //    remove { _summaryRowAdded -= value; }
+        //}
+
+
+        //public UIList()
+        //{
+        //    SummaryRow.CollectionChanged += (s, e) 
+        //        => _summaryRowAdded?.Raise();
+        //}
+
+
+        public ObservableCollection<T>  SummaryRows  { get; } = new ObservableCollection<T>();
+
+
+        public void SetItems(IEnumerable<T> items)
+        {
+            this.Clear();
+
+            foreach (var item in items)
+                this.Add(item);
+        }
+
+
+        public void SetSummary(T summaryRow)
+        {
+            SummaryRows.Clear();
+            SummaryRows.Add(summaryRow);
+        }
+    }
+}
