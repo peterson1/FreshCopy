4fb308ddb4fa55d4ac2ef97e8a82e30672d4f230
Peterson Salamat <petersonsalamat@gmail.com>
2017-10-05 06:00:45 +0800

    Started working on ServerControl project

--- CommonTools.Lib.fx45/SignalRServers/ClientStatusHub1.cs
+++ CommonTools.Lib.fx45/SignalRServers/ClientStatusHub1.cs
@@ -1,8 +1,9 @@
 ﻿using CommonTools.Lib.fx45.Cryptography;
 using CommonTools.Lib.ns11.SignalRClients;
-using CommonTools.Lib.ns11.SignalRServers;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace CommonTools.Lib.fx45.SignalRServers
@@ -13,12 +14,25 @@ namespace CommonTools.Lib.fx45.SignalRServers
     {
         public const string NAME = "ClientStatusHub1";
 
-        private CurrentHubClientsVM _clients;
+        private AuthorizeHelperV1   _clients;
+        private CurrentHubClientsVM _current;
 
 
-        public ClientStatusHub1(CurrentHubClientsVM activeHubClientsList)
+        public ClientStatusHub1(AuthorizeHelperV1 authorizeHelperV1,
+                                CurrentHubClientsVM currentHubClientsVM)
         {
-            _clients = activeHubClientsList;
+            _current = currentHubClientsVM;
+            _clients = authorizeHelperV1;
+            _clients.TargetHubName = ClientStatusHub1.NAME;
         }
+
+
+        public List<HubClientSession> GetCurrentList()
+            => _current.List.ToList();
+
+
+        public override Task OnConnected    () => _clients.Enlist(Context);
+        public override Task OnReconnected  () => _clients.Enlist(Context);
+        public override Task OnDisconnected (bool stopCalled) => _clients.Delist(Context, stopCalled);
     }
 }
