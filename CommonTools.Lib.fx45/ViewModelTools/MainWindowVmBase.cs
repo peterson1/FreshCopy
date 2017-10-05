77090e19fa82231e54c8795820366669e66fbd7d
Peterson Salamat <petersonsalamat@gmail.com>
2017-10-06 02:36:30 +0800

    Moved window event handlers to a partial class. Added draggable behavior.

--- CommonTools.Lib.fx45/ViewModelTools/MainWindowVmBase.cs
+++ CommonTools.Lib.fx45/ViewModelTools/MainWindowVmBase.cs
@@ -3,18 +3,17 @@ using CommonTools.Lib.fx45.FileSystemTools;
 using CommonTools.Lib.fx45.InputTools;
 using CommonTools.Lib.fx45.ThreadTools;
 using CommonTools.Lib.ns11.DependencyInjection;
-using CommonTools.Lib.ns11.ExceptionTools;
 using CommonTools.Lib.ns11.InputTools;
 using CommonTools.Lib.ns11.StringTools;
 using System;
 using System.ComponentModel;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace CommonTools.Lib.fx45.ViewModelTools
 {
-    public abstract class MainWindowVmBase : INotifyPropertyChanged, IComponentResolver
+    public abstract partial class MainWindowVmBase : INotifyPropertyChanged, IComponentResolver
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
@@ -37,38 +36,6 @@ namespace CommonTools.Lib.fx45.ViewModelTools
         public IR2Command   ExitCmd           { get; }
         public bool         IsBusy            { get; private set; }
         public string       BusyText          { get; private set; }
-
-
-        public void HandleWindowEvents(Window win, 
-            ILifetimeScope scope, bool hideOnWindowClose = false)
-        {
-            _scope = scope;
-            SetGlobalErrorHandlers();
-
-            win.Closing += async (s, e) =>
-            {
-                e.Cancel = true;
-                if (hideOnWindowClose)
-                    win.Hide();
-                else
-                    await ExitCmd.RunAsync();
-            };
-
-            win.DataContext = this;
-            Task.Run(async () =>
-            {
-                try
-                {
-                    OnWindowLoad();
-                    await OnWindowLoadAsync();
-                }
-                catch (Exception ex)
-                {
-                    OnError(ex, "Window Initialize");
-                }
-            });
-        }
-
 
 
         protected void StartBeingBusy(string message)
@@ -135,28 +102,6 @@ namespace CommonTools.Lib.fx45.ViewModelTools
 
         protected virtual void AppendToCaption(string text)
             => Caption = $"{CaptionPrefix}  v.{_exeVer}  :  {text}";
-
-
-        private void SetGlobalErrorHandlers()
-        {
-            Application.Current.DispatcherUnhandledException += (s, e) =>
-            {
-                e.Handled = true;
-                OnError(e.Exception, "Application.Current");
-            };
-
-            TaskScheduler.UnobservedTaskException += (s, e) =>
-            {
-                e.SetObserved();
-                OnError(e.Exception, "TaskScheduler");
-            };
-
-            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
-            {
-                OnError(e.ExceptionObject as Exception, "AppDomain.CurrentDomain");
-                // application terminates after above
-            };
-        }
 
 
         public T Resolve<T>() where T : class
---
+++ CommonTools.Lib.fx45/ViewModelTools/MainWindowVmBase_EventHandlers.cs
@@ -0,0 +1,95 @@
+﻿using Autofac;
+using CommonTools.Lib.fx45.FileSystemTools;
+using CommonTools.Lib.fx45.InputTools;
+using CommonTools.Lib.fx45.ThreadTools;
+using CommonTools.Lib.ns11.DependencyInjection;
+using CommonTools.Lib.ns11.InputTools;
+using CommonTools.Lib.ns11.StringTools;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CommonTools.Lib.fx45.ViewModelTools
+{
+    public abstract partial class MainWindowVmBase : INotifyPropertyChanged, IComponentResolver
+    {
+
+
+        public void HandleWindowEvents(Window win, 
+            ILifetimeScope scope, bool hideOnWindowClose = false)
+        {
+            _scope = scope;
+
+            HandleGlobalErrors    ();
+            HandleWindowClosing   (win, hideOnWindowClose);
+            HandleWindowMouseDown (win);
+
+            win.DataContext = this;
+            RunOnWindowLoadRoutines();
+        }
+
+
+        private static void HandleWindowMouseDown(Window win)
+        {
+            win.MouseDown += (s, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                    win.DragMove();
+            };
+        }
+
+
+        private void RunOnWindowLoadRoutines()
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    OnWindowLoad();
+                    await OnWindowLoadAsync();
+                }
+                catch (Exception ex)
+                {
+                    OnError(ex, "Window Initialize");
+                }
+            });
+        }
+
+
+        private void HandleWindowClosing(Window win, bool hideOnWindowClose)
+        {
+            win.Closing += async (s, e) =>
+            {
+                e.Cancel = true;
+                if (hideOnWindowClose)
+                    win.Hide();
+                else
+                    await ExitCmd.RunAsync();
+            };
+        }
+
+
+        private void HandleGlobalErrors()
+        {
+            Application.Current.DispatcherUnhandledException += (s, e) =>
+            {
+                e.Handled = true;
+                OnError(e.Exception, "Application.Current");
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                e.SetObserved();
+                OnError(e.Exception, "TaskScheduler");
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                OnError(e.ExceptionObject as Exception, "AppDomain.CurrentDomain");
+                // application terminates after above
+            };
+        }
+    }
+}
