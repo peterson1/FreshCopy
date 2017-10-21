ec32f15d3ada0a47dfd68c89ead96adb576727c2
Peterson Salamat <petersonsalamat@gmail.com>
2017-10-21 19:41:58 +0800

    added GetOrDefault extension for dictionaries

--- CommonTools.Lib.ns11/CollectionTools/DictionaryExtensions.cs
+++ CommonTools.Lib.ns11/CollectionTools/DictionaryExtensions.cs
@@ -17,5 +17,28 @@ namespace CommonTools.Lib.ns11.CollectionTools
             else if (errorIfMissing)
                 throw Fault.BadData($"Dictionary ‹{typeof(TVal).Name}› does not contain key [{key}].");
         }
+
+
+        //http://stackoverflow.com/a/2601501/3973863
+        public static TValue GetOrDefault<TKey, TValue>
+            (this IDictionary<TKey, TValue> dictionary,
+             TKey key,
+             TValue defaultValue = default(TValue))
+        {
+            TValue value;
+            if (key == null) return defaultValue;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public static TValue GetOrDefault<TKey, TValue>
+            (this IDictionary<TKey, TValue> dictionary,
+             TKey key,
+             Func<TValue> defaultValueProvider)
+        {
+            TValue value;
+            if (key == null) return defaultValueProvider();
+            return dictionary.TryGetValue(key, out value) ? value
+                 : defaultValueProvider();
+        }
     }
 }
