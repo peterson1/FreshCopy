7cb396495a03ba7905049db88e47b7292bd8f319
Peterson Salamat <petersonsalamat@gmail.com>
2017-10-22 10:39:30 +0800

    Added GmailSender plus tests

---
+++ CommonTools.Lib.fx45/GoogleTools/GmailSender1.cs
@@ -0,0 +1,51 @@
+﻿using CommonTools.Lib.ns11.CollectionTools;
+using CommonTools.Lib.ns11.GoogleTools;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace CommonTools.Lib.fx45.GoogleTools
+{
+    public class GmailSender1
+    {
+        private WrappingIterator<GmailSenderSettings.Credentials> _accts;
+
+        public GmailSender1(GmailSenderSettings gmailSenderSettings)
+        {
+            _accts = new WrappingIterator<GmailSenderSettings.Credentials>(gmailSenderSettings.Accounts);
+        }
+
+
+        public async Task Send(string recipientEmail, string subject, string messageBody)
+        {
+            var acct = _accts.GetNext();
+            var from = new MailAddress(acct.Email, acct.SenderName);
+            var dest = new MailAddress(recipientEmail);
+            var smtp = CreateSmtpClient(acct);
+
+            using (var msg = CreateMessage(from, dest, subject, messageBody))
+            {
+                await smtp.SendMailAsync(msg);
+            }
+        }
+
+
+        private MailMessage CreateMessage(MailAddress from, MailAddress dest, string subject, string messageBody) 
+            => new MailMessage(from, dest)
+            {
+                Subject = subject,
+                Body    = messageBody
+            };
+
+
+        private SmtpClient CreateSmtpClient(GmailSenderSettings.Credentials creds) => new SmtpClient
+        {
+            Host                  = "smtp.gmail.com",
+            Port                  = 587,
+            EnableSsl             = true,
+            DeliveryMethod        = SmtpDeliveryMethod.Network,
+            UseDefaultCredentials = false,
+            Credentials           = new NetworkCredential(creds.Email, creds.Password)
+        };
+    }
+}
---
+++ FreshCopy.Tests/GmailSendTests/GmailSender1Facts.cs
@@ -0,0 +1,40 @@
+﻿using CommonTools.Lib.fx45.FileSystemTools;
+using CommonTools.Lib.fx45.GoogleTools;
+using CommonTools.Lib.ns11.GoogleTools;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FreshCopy.Tests.GmailSendTests
+{
+    [Trait("Internet","local cfg")]
+    public class GmailSender1Facts
+    {
+        [Fact(DisplayName = "Solo Send")]
+        public async Task SoloSend()
+        {
+            var cfg = JsonFile.Read<GmailSenderSettings>("gmailSender.cfg");
+            var sut = new GmailSender1(cfg);
+            var now = DateTime.Now.ToLongTimeString();
+            var sbj = $"Sample subject {now}";
+            var msg = $"Sample message {now}";
+            await sut.Send("petersonsalamat@gmail.com", sbj, msg);
+        }
+
+
+        [Fact(DisplayName = "Multi Send")]
+        public async Task MultiSend()
+        {
+            var cfg = JsonFile.Read<GmailSenderSettings>("gmailSender.cfg");
+            var sut = new GmailSender1(cfg);
+
+            for (int i = 0; i < 6; i++)
+            {
+                var now = DateTime.Now.ToLongTimeString();
+                var sbj = $"Sample subject {now}";
+                var msg = $"Sample message {now}";
+                await sut.Send("petersonsalamat@gmail.com", sbj, msg);
+            }
+        }
+    }
+}
