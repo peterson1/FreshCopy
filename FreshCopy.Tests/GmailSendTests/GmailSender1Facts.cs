41d2cb5e2de3d8a963645ce58eb9bf092c8942a5
Peterson Salamat <petersonsalamat@gmail.com>
2017-10-30 18:13:28 +0800

    Skipped Gmail tests

--- FreshCopy.Tests/GmailSendTests/GmailSender1Facts.cs
+++ FreshCopy.Tests/GmailSendTests/GmailSender1Facts.cs
@@ -10,19 +10,19 @@ namespace FreshCopy.Tests.GmailSendTests
     [Trait("Internet","local cfg")]
     public class GmailSender1Facts
     {
-        [Fact(DisplayName = "Solo Send")]
-        public async Task SoloSend()
-        {
-            var cfg = JsonFile.Read<GmailSenderSettings>("gmailSender.cfg");
-            var sut = new GmailSender1(cfg);
-            var now = DateTime.Now.ToLongTimeString();
-            var sbj = $"Sample subject {now}";
-            var msg = $"Sample message {now}";
-            await sut.Send("petersonsalamat@gmail.com", sbj, msg);
-        }
+        //[Fact(DisplayName = "Solo Send")]
+        //public async Task SoloSend()
+        //{
+        //    var cfg = JsonFile.Read<GmailSenderSettings>("gmailSender.cfg");
+        //    var sut = new GmailSender1(cfg);
+        //    var now = DateTime.Now.ToLongTimeString();
+        //    var sbj = $"Sample subject {now}";
+        //    var msg = $"Sample message {now}";
+        //    await sut.Send("petersonsalamat@gmail.com", sbj, msg);
+        //}
 
 
-        [Fact(DisplayName = "Multi Send")]
+        [Fact(DisplayName = "Multi Send", Skip = "too spammy")]
         public async Task MultiSend()
         {
             var cfg = JsonFile.Read<GmailSenderSettings>("gmailSender.cfg");
