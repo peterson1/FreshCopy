f8b269bbffa3828e781ef34bb2047b44ad10896e
Pete @BK1 Svr <petersonsalamat@gmail.com>
2017-12-15 14:07:01 +0800

    Upload now works

--- FreshCopy.FirebaseUploader.WPF/FilePicker/CurrentFileVM.cs
+++ FreshCopy.FirebaseUploader.WPF/FilePicker/CurrentFileVM.cs
@@ -3,9 +3,9 @@ using CommonTools.Lib.fx45.InputTools;
 using CommonTools.Lib.fx45.ViewModelTools;
 using CommonTools.Lib.ns11.InputTools;
 using FreshCopy.FirebaseUploader.WPF.WebAccess;
-using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace FreshCopy.FirebaseUploader.WPF.FilePicker
@@ -17,38 +17,42 @@ namespace FreshCopy.FirebaseUploader.WPF.FilePicker
         public CurrentFileVM(FirebaseIO firebaseIO)
         {
             _fBase = firebaseIO;
-            UploadCmd = R2Command.Async(DoUpload, _ => CanUpload(), "Upload");
+            UploadCmd  = R2Command.Async(DoUpload, _ => CanUpload(), "Upload");
+            RefreshCmd = R2Command.Async(CompareWithRemote, _ => !IsBusy, "Refresh");
         }
 
 
-        public string      Key            { get; private set; }
-        public string      Path           { get; private set; }
+        public string      FileID         { get; private set; }
+        public string      LocalPath      { get; private set; }
         public bool        Found          { get; private set; }
         public string      LocalSHA1      { get; private set; }
         public string      LocalVersion   { get; private set; }
         public string      RemoteSHA1     { get; private set; }
         public string      RemoteVersion  { get; private set; }
+        public string      DownloadURL    { get; private set; }
         public bool        IsSame         { get; private set; }
         public IR2Command  UploadCmd      { get; }
+        public IR2Command  RefreshCmd     { get; }
 
 
         internal async Task LoadFile(string fileKey, string filePath)
         {
-            if (IsBusy) return;
-            Key           = fileKey;
-            Path          = filePath;
-            Found         = File.Exists(Path);
-            LocalSHA1     = Found ? Path.SHA1ForFile() : "--";
-            LocalVersion  = Found ? Path.GetVersion()  : "--";
+            FileID        = fileKey;
+            LocalPath     = filePath;
+            Found         = File.Exists(LocalPath);
+            LocalSHA1     = Found ? LocalPath.SHA1ForFile() : "--";
+            LocalVersion  = Found ? LocalPath.GetVersion()  : "--";
             RemoteSHA1    = "--";
             RemoteVersion = "--";
+            DownloadURL   = "--";
             IsSame        = true;
-            if (Found) await CompareWithRemote();
+            await RefreshCmd.RunAsync();
         }
 
 
         private async Task CompareWithRemote()
         {
+            if (!Found) return;
             StartBeingBusy("Comparing with remote file ...");
             await GetRemoteFileInfo();
             IsSame = LocalSHA1 == RemoteSHA1;
@@ -59,7 +63,7 @@ namespace FreshCopy.FirebaseUploader.WPF.FilePicker
 
         private async Task GetRemoteFileInfo()
         {
-            var node = await _fBase.GetInfo(Key);
+            var node = await _fBase.GetInfo(FileID);
             if (node == null) return;
             RemoteSHA1    = node.SHA1;
             RemoteVersion = node.Version;
@@ -76,9 +80,31 @@ namespace FreshCopy.FirebaseUploader.WPF.FilePicker
 
         private async Task DoUpload()
         {
-            StartBeingBusy("Uploading ...");
-            await Task.Delay(1000);
-            StopBeingBusy();
+            var fnme = System.IO.Path.GetFileName(LocalPath);
+            StartBeingBusy($"Uploading “{fnme}” ...");
+
+            DownloadURL = await _fBase.Upload(LocalPath);
+            var ok = await VerifyUploadedFile(DownloadURL);
+            if (!ok) return;
+
+            await _fBase.UpdateNode(this);
+            await RefreshCmd.RunAsync();
         }
+
+
+        private async Task<bool> VerifyUploadedFile(string url)
+        {
+            StartBeingBusy("Verifying uploaded file ...");
+
+            var isOk = await _fBase.VerifyUpload(url, LocalSHA1);
+            if (isOk)
+                MessageBox.Show("File maintained its integrity", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("File got CORRUPTED", "FAIL", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            StopBeingBusy();
+            return isOk;
+        }
+
     }
 }
--- FreshCopy.FirebaseUploader.WPF/WebAccess/FirebaseIO.cs
+++ FreshCopy.FirebaseUploader.WPF/WebAccess/FirebaseIO.cs
@@ -1,6 +1,10 @@
-﻿using CommonTools.Lib.fx45.FirebaseTools;
+﻿using CommonTools.Lib.fx45.FileSystemTools;
+using CommonTools.Lib.fx45.FirebaseTools;
 using CommonTools.Lib.ns11.DataStructures;
 using FreshCopy.FirebaseUploader.WPF.Configuration;
+using FreshCopy.FirebaseUploader.WPF.FilePicker;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace FreshCopy.FirebaseUploader.WPF.WebAccess
@@ -23,5 +27,35 @@ namespace FreshCopy.FirebaseUploader.WPF.WebAccess
 
         private FirebaseConnection Conn
             => new FirebaseConnection(_loadr.CurrentCfg.FirebaseCreds);
+
+
+        internal Task<string> Upload(string filePath)
+            => Conn.UploadFile(filePath);
+
+
+        internal Task UpdateNode(CurrentFileVM vm)
+            => Conn.UpdateNode(new PublicFileInfo
+            {
+                DownloadURL = vm.DownloadURL,
+                Version     = vm.LocalVersion,
+                SHA1        = vm.LocalSHA1
+            },
+            ROOT, vm.FileID, SUBKEY);
+
+
+        internal async Task<bool> VerifyUpload(string url, string sha1)
+        {
+            var tmp = Path.GetTempFileName();
+
+            using (var wc = new WebClient())
+                await wc.DownloadFileTaskAsync(url, tmp);
+
+            await Task.Delay(1000);
+
+            var isOK = tmp.SHA1ForFile() == sha1;
+            File.Delete(tmp);
+
+            return isOK;
+        }
     }
 }
