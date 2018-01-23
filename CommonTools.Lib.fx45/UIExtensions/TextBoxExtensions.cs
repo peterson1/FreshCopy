07a8b4ee427e2c54156868b91d8264de705f81dd
Peterson Salamat <petersonsalamat@gmail.com>
2018-01-24 04:49:32 +0800

    Added TextBox validation extensions

---
+++ CommonTools.Lib.fx45/UIExtensions/TextBoxExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CommonTools.Lib.fx45.ValidationRules;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CommonTools.Lib.fx45.UIExtensions
+{
+    public static class TextBoxExtensions
+    {
+        public static void ValidateNonBlank(this TextBox txt)
+        {
+            if (txt.Tag == null) return;
+            var b = new Binding(txt.Tag.ToString());
+            b.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            b.ValidationRules.Add(new NotBlankValidationRule());
+            txt.SetBinding(TextBox.TextProperty, b);
+        }
+    }
+}
