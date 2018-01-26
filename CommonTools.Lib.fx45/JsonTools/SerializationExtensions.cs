391dfc6398fb4740983dd0cc19c51fd50e9df1c5
Pete @BK1 Svr <petersonsalamat@gmail.com>
2018-01-26 16:49:24 +0800

    Added object extension: SHA1OfJson

---
+++ CommonTools.Lib.fx45/JsonTools/SerializationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CommonTools.Lib.ns11.StringTools;
+using Newtonsoft.Json;
+
+namespace CommonTools.Lib.fx45.JsonTools
+{
+    public static class SerializationExtensions
+    {
+        public static string SHA1OfJson(this object obj)
+            => JsonConvert.SerializeObject(obj).SHA1ForUTF8();
+    }
+}
