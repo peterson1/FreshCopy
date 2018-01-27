a8910558012646d7c7c7aad3e6b0f804bdb87bf3
Peterson Salamat <petersonsalamat@gmail.com>
2018-01-28 04:23:30 +0800

    Added simplistic Pluralize extension along with unit tests for it.

---
+++ CommonTools.Lib.ns11/LanguageTools/PluralizeExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonTools.Lib.ns11.LanguageTools
+{
+    public static class PluralizeExtensions
+    {
+        public static string Pluralize(this string singular, object number)
+        {
+            if (number == null) return $"NULL {singular}s";
+
+            if (!int.TryParse(number.ToString(), out int num))
+                return $"NaN {singular}s";
+
+            var sufx = num == 1 ? singular : $"{singular}s";
+            return $"{num:N0} {sufx}";
+        }
+    }
+}
---
+++ FreshCopy.Tests/LanguageToolsTests/PluralizeExtensionFacts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonTools.Lib.ns11.LanguageTools;
+using FluentAssertions;
+using Xunit;
+
+namespace FreshCopy.Tests.LanguageToolsTests
+{
+    [Trait("Batch", "1")]
+    [Trait("Pluralize", "Solitary")]
+    public class PluralizeExtensionFacts
+    {
+        [Theory]
+        [InlineData("dog", 0, "0 dogs")]
+        [InlineData("dog", 1, "1 dog")]
+        [InlineData("dog", 2, "2 dogs")]
+        [InlineData("dog", 1234, "1,234 dogs")]
+        [InlineData("dog", null, "NULL dogs")]
+        [InlineData("dog", "abc", "NaN dogs")]
+        //[InlineData("dog", null, "dogs")]
+        public void TestMethod(string singular, object count, string expctd)
+        {
+            singular.Pluralize(count).Should().Be(expctd);
+        }
+    }
+}
