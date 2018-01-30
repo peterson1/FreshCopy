1cbb329f2dad152cf407576979d25dde2ab9e818
Peterson Salamat <petersonsalamat@gmail.com>
2018-01-30 16:19:40 +0800

    Added HumanName property to Firebase creds

--- CommonTools.Lib.ns11/GoogleTools/FirebaseCredentials.cs
+++ CommonTools.Lib.ns11/GoogleTools/FirebaseCredentials.cs
@@ -6,5 +6,6 @@
         public string   ApiKey     { get; set; }
         public string   Email      { get; set; }
         public string   Password   { get; set; }
+        public string   HumanName  { get; set; }
     }
 }
