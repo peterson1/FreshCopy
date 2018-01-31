9cbd6635413ca4fa9afbb83a52ccf31040af47e1
Pete @BK1 Svr <petersonsalamat@gmail.com>
2018-01-31 08:51:45 +0800

    Removed HumanName property from FirebaseCreds -- that should be on a subclass

--- CommonTools.Lib.ns11/GoogleTools/FirebaseCredentials.cs
+++ CommonTools.Lib.ns11/GoogleTools/FirebaseCredentials.cs
@@ -6,6 +6,5 @@
         public string   ApiKey     { get; set; }
         public string   Email      { get; set; }
         public string   Password   { get; set; }
-        public string   HumanName  { get; set; }
     }
 }
