ac7fcc3191f6f739942f4de42cc6eed7dcd5cbf6
Peterson Salamat <petersonsalamat@gmail.com>
2018-02-08 19:29:18 +0800

    Added FSharp option extensions

---
+++ CommonTools.Lib.fx45/FSharpTools/FSharpOptionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.FSharp.Core;
+using System;
+
+namespace CommonTools.Lib.fx45.FSharpTools
+{
+    public static class FSharpOptionExtensions
+    {
+        public static FSharpOption<T> Create<T>(T value)
+            => new FSharpOption<T>(value);
+
+        public static bool IsSome<T>(this FSharpOption<T> opt)
+            => FSharpOption<T>.get_IsSome(opt);
+
+        public static bool IsNone<T>(this FSharpOption<T> opt)
+            => FSharpOption<T>.get_IsNone(opt);
+
+        public static Nullable<T> ToNullable<T>(this FSharpOption<T> opt)
+            where T : struct
+            => opt.IsSome() ? opt.Value : (Nullable<T>)null;
+    }
+}
