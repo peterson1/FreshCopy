8322ee6ab6593641cb6846eccc9525c1dcec4ff3
Peterson Salamat <petersonsalamat@gmail.com>
2018-04-27 05:28:00 +0800

    Updated libraries to latest Firebase nugets

--- FreshCopy.UpdateChecker.WPF/Properties/AssemblyInfo.cs
+++ FreshCopy.UpdateChecker.WPF/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.18090.0109")]
+[assembly: AssemblyFileVersion("1.0.18116.2119")]
